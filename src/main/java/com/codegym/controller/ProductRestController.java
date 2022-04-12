882e55c1360af961a09582830007527b886b5a5a
Toan-246 <n.v.toan687@gmail.com>
2022-04-12 11:49:29 +0700

    jwt

--- src/main/java/com/codegym/controller/ProductRestController.java
+++ src/main/java/com/codegym/controller/ProductRestController.java
@@ -1,54 +1,98 @@
 package com.codegym.controller;
 
 import com.codegym.model.Product;
+import com.codegym.model.ProductForm;
 import com.codegym.service.product.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/products")
 public class ProductRestController {
+    @Value("${file-upload}")//giá trị trong file upload-file.properties
+    private String uploadPath;
     @Autowired
     private IProductService productService;
+
     @GetMapping
-    public ResponseEntity<Iterable<Product>>findAll(@RequestParam(name = "q")Optional<String>q){
+    public ResponseEntity<Iterable<Product>> findAll(@RequestParam(name = "q") Optional<String> q) {
         Iterable<Product> products = productService.findAll();
-        if (q.isPresent()){
+        if (q.isPresent()) {
             products = productService.findAllByNameContaining(q.get());
         }
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
+
     @GetMapping("/{id}")
-    public ResponseEntity<Product> findById(@PathVariable Long id){
+    public ResponseEntity<Product> findById(@PathVariable Long id) {
         Optional<Product> product = productService.findById(id);
-        if (!product.isPresent()){
+        if (!product.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(product.get(), HttpStatus.OK);
     }
+
     @PostMapping
-    public ResponseEntity<Product> saveProduct (@RequestBody Product product){
-        return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
-    }
-    @PutMapping ("/{id}")
-    public ResponseEntity<Product> updateProduct (@PathVariable Long id,@RequestBody Product newProduct){
-        Optional<Product> optionalProduct = productService.findById(id);
-        if (!optionalProduct.isPresent()){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<Product> saveProduct(@ModelAttribute ProductForm productForm) {
+        MultipartFile img = productForm.getImage();
+        if (img.getSize() != 0) {
+            String fileName = img.getOriginalFilename();
+            long currentTime = System.currentTimeMillis();
+            fileName = currentTime + fileName;
+            try {
+                FileCopyUtils.copy(img.getBytes(), new File(uploadPath + fileName));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Product product = new Product(productForm.getId(), productForm.getName(), productForm.getPrice(), productForm.getDescription(), fileName, productForm.getCategory());
+            return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
         }
-        newProduct.setId(optionalProduct.get().getId());
-        return new ResponseEntity<>(productService.save(newProduct), HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
-    @DeleteMapping ("/{id}")
-    public ResponseEntity<Product> deleteProduct (@PathVariable Long id){
+    @PostMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @ModelAttribute ProductForm productForm) {
         Optional<Product> optionalProduct = productService.findById(id);
-        if (!optionalProduct.isPresent()){
+        MultipartFile img = productForm.getImage();
+        if (optionalProduct.isPresent()) {
+            Product oldProduct = optionalProduct.get();
+            if (img.getSize() != 0) {
+                String fileName = img.getOriginalFilename();
+                long currentTime = System.currentTimeMillis();
+                fileName = currentTime + fileName;
+                oldProduct.setImage(fileName);
+                try {
+                    FileCopyUtils.copy(img.getBytes(), new File(uploadPath + fileName));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            oldProduct.setId(productForm.getId());
+            oldProduct.setName(productForm.getName());
+            oldProduct.setPrice(productForm.getPrice());
+            oldProduct.setDescription(productForm.getDescription());
+            oldProduct.setCategory(productForm.getCategory());
+            return new ResponseEntity<>(productService.save(oldProduct), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable Long id) {
+        Optional<Product> optionalProduct = productService.findById(id);
+        if (!optionalProduct.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         productService.deleteById(id);
---
+++ src/main/java/com/codegym/model/User.java
@@ -0,0 +1,49 @@
+package com.codegym.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    @ManyToMany (fetch = FetchType.EAGER)//Để load a các role để cho JWT check role, nếu để LAZY sẽ không có role để check
+    @JoinTable(name = "user_role")
+    private List<Role> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+}
